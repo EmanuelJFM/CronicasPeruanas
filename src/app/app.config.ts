a3e47e627f190f9d8e4037f611096637133bb200
EmanuelJFM <emanuel.2211.noviembre@gmial.com>
2024-05-21 15:25:18 -0500

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp } from '@angular/fire/app'
+import { provideStorage } from '@angular/fire/storage'
+import { routes } from './app.routes';
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({
+    apiKey: "AIzaSyDYhDIwBhQRW4z7SHApCyArHvab-BRG6Kk",
+    authDomain: "pagina-noticias-e72ca.firebaseapp.com",
+    projectId: "pagina-noticias-e72ca",
+    storageBucket: "pagina-noticias-e72ca.appspot.com",
+    messagingSenderId: "30010539765",
+    appId: "1:30010539765:web:a30ef036d4f72c6dba8fbc"
+  })),
+  provideStorage(() => getStorage()), provideAnimationsAsync()
+]
+};
